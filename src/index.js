aa0db96f77d7e4d32f1156ab5fbfb3bda56c9bd1
Michael Wood <michaelwood33311@icloud.com>
2023-03-09 13:06:27 -0500

    added subscribe/unsubscribe

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import store from "./store";
 import { addTask, removeTask } from './action';
 
-console.log(store.getState());
-
-store.subscribe(() => {
+const unsubscribe = store.subscribe(() => {
     console.log('updated state', store.getState());
 })
 
 store.dispatch(addTask('Buy Bread'));
 
-console.log(store.getState());
-
 store.dispatch(removeTask(1));
 
-console.log(store.getState());
+unsubscribe();
