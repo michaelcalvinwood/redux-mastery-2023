11c9af4892a051ccfbda7b711b61cc07af999fed
Michael Wood <michaelwood33311@icloud.com>
2023-03-10 08:05:43 -0500

    added implementing redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 import taskReducer from './tasks';
 import employeesReducer from './employees';
 import log from '../middleware/log';
+import logger from 'redux-logger';
 
 const store = configureStore({ 
     reducer: {
         tasks: taskReducer,
         employees: employeesReducer
     },
-    middleware: [log] 
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger]
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
